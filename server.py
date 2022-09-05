eb50d6540fbdd58cd5d7ba2d2088d96515db46c9
Faisal's Macbook Pro <faisal@Faisalss-MacBook-Pro.local>
2022-09-05 16:31:14 -0700

    made final changes

--- server.py
+++ server.py
@@ -1,12 +1,8 @@
-import json
 import os
-import io
-import PIL.Image as Image
+import base64
 from pathlib import Path
 from flask import Flask
 from flask import request
-from array import array
-
 
 app = Flask(__name__)
 
@@ -20,15 +16,17 @@ def image_upload():
     category = request_data["category"]
     imageData = request_data["imageData"]
     print('str',imageData)
-    newFileBytes = bytes(imageData,encoding='utf-8')
     Path(f"./Images/{category}").mkdir(parents=True, exist_ok=True)
     numFiles = len([name for name in os.listdir('.') if os.path.isfile(name)])
-    # newFile = open(f"{numFiles+1}.png", "wb")
-    # newFile.write(newFileByteArray)
+    try:
+        with open(f"./Images/{category}/{numFiles+1}.jpeg", "wb") as fh:
+            fh.write(base64.decodebytes(imageData.encode()))
+    except:
+        print("Exception occured")
+        return {
+            "statusCode": 200
+        }
 
-    # bytes = readimage(f"./Images/{category}.png")
-    image = Image.open(io.BytesIO(newFileBytes))
-    image.save(f"./Images/{category}/{numFiles+1}/.png")
     return {
         "statusCode": 200
     }
