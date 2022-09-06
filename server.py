e526c122d6a7df08142afc16be16aceb9bc4b626
Faisal's Macbook Pro <faisal@Faisalss-MacBook-Pro.local>
2022-09-05 22:40:07 -0700

    Removed redundant code - Faisal

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import base64
 from pathlib import Path
 from flask import Flask
 from flask import request
+import time
 
 app = Flask(__name__)
 
@@ -15,11 +16,9 @@ def image_upload():
     request_data = request.get_json()
     category = request_data["category"]
     imageData = request_data["imageData"]
-    print('str',imageData)
     Path(f"./Images/{category}").mkdir(parents=True, exist_ok=True)
-    numFiles = len([name for name in os.listdir('.') if os.path.isfile(name)])
     try:
-        with open(f"./Images/{category}/{numFiles+1}.jpeg", "wb") as fh:
+        with open(f"./Images/{category}/{time.time()}.jpeg", "wb") as fh:
             fh.write(base64.decodebytes(imageData.encode()))
     except:
         print("Exception occured")
