6fa16167dccc83f53ac0d12be7d8868a313a10bd
mouryateja95 <mouryateja95@gmail.com>
2022-12-06 18:17:26 -0700

    Made changes to send array in server response

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ pip install Flask
 flask --app server run --host=0.0.0.0
 
 python -m venv mc-flask
-mc-flask\Scripts\activate
+mc-flask/Scripts/activate
+. mc-flask\Scripts\activate
 pip install tensorflow
 pip install opencv-python
 pip install pyyaml h5py 
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from flask import Flask,current_app
 from flask import request
 import time
 
+import json
 import tensorflow as tf
 import numpy as np
 import cv2
@@ -13,14 +14,27 @@ from keras import models, layers
 from keras.datasets import mnist
 from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D, MaxPool2D, BatchNormalization
 from keras.models import Sequential
+totalPred =  [0] * 10
+totalClassification = [""] * 10
+counter = 0
 cmodel = ""
+topleft = ""
+topright = ""
+bottomleft = ""
+bottomright = ""
 classification = -1
+counter = 0
 app = Flask(__name__)
 
 @app.before_first_request
 def load_model():
 	global cmodel
-	file_name = "model.h5"
+	global topleft
+	global topright
+	global bottomleft
+	global bottomright
+	# file_name = "model.h5"
+	file_name = "trained_model_topleft.h5"
 	if not os.path.isfile(file_name):
 		(x_train, y_train) , (x_test, y_test) = mnist.load_data()
 
@@ -59,7 +73,11 @@ def load_model():
 
 	# return pickle.load(open(file_name, "rb"))
 		# return tf.keras.models.load_model(file_name)
-	cmodel = tf.keras.models.load_model(file_name)
+	# cmodel = tf.keras.models.load_model(file_name)
+	topleft = tf.keras.models.load_model(file_name)
+	topright = tf.keras.models.load_model("trained_model_topright.h5")
+	bottomleft = tf.keras.models.load_model("trained_model_bottomleft.h5")
+	bottomright = tf.keras.models.load_model("trained_model_bottomright.h5")
 
 
 	# model = load_model("model.h5")
@@ -91,12 +109,13 @@ def get_file_type(image_bytes):
 # tf.keras.models.load_model("model.h5")
 
 def predict_digit(model, image_bytes): # takes image file name and classifies it
-	img_size = 28
+	global counter,totalPred
+	img_size = 14
 
 	np_array = np.frombuffer(image_bytes, np.uint8)
 	img = cv2.imdecode(np_array, -1)
 	# print("img.shape", img.shape)
-	img = cv2.resize(img,(28,28))
+	img = cv2.resize(img,(img_size,img_size))
 	grayImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # greyscale
 	# cv2.imshow("Gray",grayImage)
 	# cv2.waitKey(0) 
@@ -106,13 +125,22 @@ def predict_digit(model, image_bytes): # takes image file name and classifies it
 	
 	# print("grayImage.shape", grayImage.shape)
 	grayImage = ~grayImage # invert image like in dataset
-	(thresh, grayImage) = cv2.threshold(grayImage, 200, 255, cv2.THRESH_BINARY)
+	(thresh, grayImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
 	resizedImage = cv2.resize(grayImage, (img_size, img_size), interpolation = cv2.INTER_AREA) # resize image
 	finalImage = tf.keras.utils.normalize(resizedImage, axis = 1)
 	finalImage = np.array(finalImage).reshape(-1, img_size, img_size, 1)
 	predictions = model.predict(finalImage)
-	# print("predictions")
-	return np.argmax(predictions)
+	return predictions
+
+def getModel(position):
+	if position == 0:
+		return topleft
+	elif position == 1:
+		return topright
+	elif position == 2:
+		return bottomleft
+	else:
+		return bottomright
 
 @app.route("/")
 def server_home():
@@ -122,17 +150,23 @@ def server_home():
 def image_upload():
 	global cmodel
 	global classification
+	global totalPred
+	global counter
+	global totalClassification
 	request_data = request.get_json()
 	imageData = request_data["imageData"]
-
+	position = request_data["position"]
+	
 	# decode the image
 	try:
 		decoded = base64.decodebytes(imageData.encode())
 		# print("decoded: ",decoded)
 		
 		# print("cmodel",cmodel)
-		classification = str(predict_digit(cmodel, decoded))
-		print("classification",classification)
+		predictions = predict_digit(getModel(position=position), decoded)
+		classification = str(np.argmax(predictions))
+		
+		
 		extension = get_file_type(decoded)
 	except Exception as e: 
 		print (str(e))
@@ -141,26 +175,27 @@ def image_upload():
 			"statusCode": 500
 		}
 
-	# ensure that we are not overwriting any files
-	destination = f"./Images/{classification}"
-	Path(destination).mkdir(parents = True, exist_ok = True)
-	numFiles = len([name for name in os.listdir(destination) if os.path.isfile(f"{destination}/{name}")])
+	# # ensure that we are not overwriting any files
+	# destination = f"./Images/{position}"
+	# Path(destination).mkdir(parents = True, exist_ok = True)
+	# numFiles = len([name for name in os.listdir(destination) if os.path.isfile(f"{destination}/{name}")])
 
-	potential_name = numFiles + 1
-	while os.path.isfile(f"{destination}/{potential_name}.{extension}"):
-		potential_name += 1
+	# potential_name = numFiles + 1
+	# while os.path.isfile(f"{destination}/{potential_name}.{extension}"):
+	# 	potential_name += 1
 
 	# write the file to disk
-	try:
-		with open(f"{destination}/{potential_name}.{extension}", "wb") as fh:
-			fh.write(base64.decodebytes(imageData.encode()))
-	except:
-		print("Exception occured")
-		return {
-			"statusCode": 500
-		}
+	# try:
+	# 	with open(f"{destination}/{potential_name}.{extension}", "wb") as fh:
+	# 		fh.write(base64.decodebytes(imageData.encode()))
+	# except:
+	# 	print("Exception occured")
+	# 	return {
+	# 		"statusCode": 500
+	# 	}
 
 	return {
 		"statusCode": 200,
+		"predictions": predictions.tolist(),
 		"classification": classification
 	}
